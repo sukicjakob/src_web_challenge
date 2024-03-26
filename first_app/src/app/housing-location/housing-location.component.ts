d31a3915a23e8bdd29f06d0aa9d84a2b56948598
sukicjakob <sukic.jakob@gmail.com>
2024-03-26 10:08:18 +0100

    housing location component input, property binding & interpolation

--- first_app/src/app/housing-location/housing-location.component.ts
+++ first_app/src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section>
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      <p>
+    </section>
   `,
   styleUrl: './housing-location.component.css'
 })
-export class HousingLocationComponent {
 
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
