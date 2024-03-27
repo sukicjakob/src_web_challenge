63e1e42a25daa53e749d3674bf385de46c6e3b86
sukicjakob <sukic.jakob@gmail.com>
2024-03-27 19:57:21 +0100

    1. search & filter products, 2. set limit, skip & select settings for products

--- web-challenge/src/app/pages/my-pages/products/products.component.ts
+++ web-challenge/src/app/pages/my-pages/products/products.component.ts
@@ -11,11 +11,49 @@ export class AppProductsComponent implements OnInit {
 
   @Input() product: Product;
   productsList: Product[];
-  
+  filteredProductsList: Product[];
+  searchValue: string = "";
+  limitValue: number = 0;
+  skipValue: number = 0;
+  selectValue: string = "";
+
   constructor(private productsService: ProductsService) {
-    this.productsService.getAllProducts().subscribe(res => {
-      this.productsList = res.products ?? []; 
+    this.getAllProducts();
+  }
+
+  getAllProducts(){
+    this.productsService.getAllProducts(this.limitValue, this.skipValue, this.selectValue).subscribe(res => {
+      this.productsList = res.products ?? [];
+      this.filteredProductsList = this.productsList; 
     })
+  }
+
+  searchProducts(){
+    this.searchValue = (<HTMLInputElement>document.getElementById("searchInputField")).value;
+
+    if(!this.searchValue){
+      this.ngOnInit();
+      return;
+    }
+
+    this.productsService.searchProducts(this.searchValue).subscribe(res => {
+    this.filteredProductsList = res.products ?? []});
+  }
+
+  filterProducts(text: string){
+    if(!text){
+      this.filteredProductsList = this.productsList;
+      return;
+    }
+
+    this.filteredProductsList = this.productsList.filter(product => product?.title.toLowerCase().includes(text.toLowerCase()));
+  }
+
+  applySettings(){
+    this.limitValue = parseInt((<HTMLInputElement>document.getElementById("limitInputField")).value);
+    this.skipValue = parseInt((<HTMLInputElement>document.getElementById("skipInputField")).value);
+    this.selectValue = (<HTMLInputElement>document.getElementById("selectInputField")).value;
+    this.getAllProducts();
   }
 
   ngOnInit(): void {}
--- web-challenge/src/app/services/products.service.ts
+++ web-challenge/src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Product, ProductsResponse } from '../pages/my-pages/products/product';
 import { Observable } from 'rxjs';
 
@@ -13,11 +13,23 @@ export class ProductsService {
 
   constructor(private http: HttpClient) {}
 
-  getAllProducts(): Observable<ProductsResponse>{
-    return this.http.get<ProductsResponse>(this.baseUrl + "?limit=0");
+  getAllProducts(limit: number, skip: number, select: string): Observable<ProductsResponse>{
+    let params = "?limit=" + limit;
+    
+    if(skip > 0)
+      params += "&skip=" + skip;
+
+    if(select)
+      params += "&select=" + select;
+
+    return this.http.get<ProductsResponse>(this.baseUrl + params);
   }
 
   getProduct(productId: number): Observable<Product>{
     return this.http.get<Product>(this.baseUrl + "/" + productId);
+  }
+
+  searchProducts(query: string): Observable<ProductsResponse>{
+    return this.http.get<ProductsResponse>(this.baseUrl + "/search?q=" + query);
   }
 }
