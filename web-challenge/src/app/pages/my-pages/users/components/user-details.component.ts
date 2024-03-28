ca70826542afa9d8c0e17d1ae7fd7aab078dae31
sukicjakob <sukic.jakob@gmail.com>
2024-03-28 20:25:29 +0100

    update & delete users

--- web-challenge/src/app/pages/my-pages/users/components/user-details.component.ts
+++ web-challenge/src/app/pages/my-pages/users/components/user-details.component.ts
@@ -7,8 +7,11 @@ import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { Post } from '../models/post';
+import { Address } from '../models/address';
 import { Todo } from '../models/todo';
 import { Product } from '../../products/models/product';
+import { Bank } from '../models/bank';
+import { Company } from '../models/company';
 
 @Component({
   selector: 'app-user-details',
@@ -19,6 +22,10 @@ import { Product } from '../../products/models/product';
 export class AppUserDetailsComponent implements OnInit {
 
   @Input() user: User = {} as User;
+  @Input() userAddress: Address = {} as Address;
+  @Input() userBank: Bank = {} as Bank;
+  @Input() userCompany: Company = {} as Company;
+
   @Input() userCarts: Cart[] = [];
   @Input() productsInCart: Product[] = [];
   @Input() userPosts: Post[] = [];
@@ -26,27 +33,44 @@ export class AppUserDetailsComponent implements OnInit {
 
   route: ActivatedRoute = inject(ActivatedRoute);
   userId = -1;
+  selectedGender = "";
+  updatingUser = false;
+  userDeleted = false;
+
   imports: [
     CommonModule,
     ReactiveFormsModule
   ];
 
-  updateProductForm = new FormGroup({
-    title: new FormControl(''),
-    description: new FormControl(''),
-    price: new FormControl(0),
-    discountPercentage: new FormControl(0.0),
-    rating: new FormControl(0),
-    stock: new FormControl(0),
-    brand: new FormControl(''),
-    category: new FormControl(''),
+  updateUserForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    birthDate: new FormControl(''),
+    gender: new FormControl(''),
+    height: new FormControl(0),
+    weight: new FormControl(0),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    address: new FormControl(''),
+    postalCode: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl('')
   })
 
   constructor(private usersService: UsersService) {
     this.userId = Number(this.route.snapshot.params['id']);
+
+    if(!this.updatingUser)
+      this.updateUserForm.disable();
+
     this.usersService.getUser(this.userId).subscribe(res => {
       this.user = res || {}; 
+      this.userAddress = res.address;
+      this.userBank = res.bank;
+      this.userCompany = res.company;
+      this.selectedGender = this.user.gender;
     });
+
     this.usersService.getUsersCarts(this.userId).subscribe(res => {
       this.userCarts = res.carts ?? [];
     });
@@ -56,6 +80,52 @@ export class AppUserDetailsComponent implements OnInit {
     this.usersService.getUsersTodos(this.userId).subscribe(res => {
       this.userTodos = res.todos ?? [];
     })
+  }
+
+  submitUpdateUser(){
+    this.updatingUser = !this.updatingUser;
+    console.log(this.updatingUser);
+
+    if(!this.updatingUser){
+      this.updateUserForm.disable();
+      let address: Address = {
+        "address": this.updateUserForm.value.address ?? '',
+        "city": this.updateUserForm.value.city ?? '',
+        "state": this.updateUserForm.value.state ?? '',
+        "postalCode": this.updateUserForm.value.postalCode ?? '',
+        "coordinates": {"lat": 0, "lng": 0}
+      };
+  
+      let user: User = {
+        id: this.userId,
+        firstName: this.updateUserForm.value.firstName ?? '',
+        lastName: this.updateUserForm.value.lastName ?? '',
+        birthDate: this.updateUserForm.value.birthDate ?? '',
+        gender: this.updateUserForm.value.gender ?? '',
+        height: this.updateUserForm.value.height ?? 0,
+        weight: this.updateUserForm.value.weight ?? 0,
+        email: this.updateUserForm.value.email ?? '',
+        phone: this.updateUserForm.value.phone ?? '',
+        address: address,
+        image: '',
+        bank: this.userBank,
+        company: this.userCompany
+      };
+  
+      this.usersService.updateUser(user).subscribe(res => {
+        this.userDeleted = res.isDeleted ?? false;
+      });
+
+      return;
+    }
+    this.updateUserForm.enable();
+  }
+
+  deleteUser(){
+    this.usersService.deleteUser(this.user.id!).subscribe(res => {
+      this.userDeleted = res.isDeleted ?? false;
+      this.ngOnInit();
+    });
   }
 
   ngOnInit(): void {
